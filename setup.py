2efbcab8357c5e936105632a2269fa60e7dd0359
Dmitriy Ivanov <d.k.ivanov@live.com>
2018-08-16 16:47:04 +0300

    add force flad for git tag

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ class UploadCommand(Command):
         os.system('twine upload dist/*')
 
         self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git tag --force v{0}'.format(about['__version__']))
         os.system('git push --tags')
 
         sys.exit()
